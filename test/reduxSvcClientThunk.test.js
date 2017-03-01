7c7d2c22d65f12ea4f5ca31043521e363dd8820c
Will Vaughn <willieviseoae@gmail.com>
2017-03-01 14:44:07 -0800

    lint

--- test/reduxSvcClientThunk.test.js
+++ test/reduxSvcClientThunk.test.js
@@ -1,6 +1,3 @@
-const assert = require('assert');
-const reduxSvcClientThunk = require('../lib/reduxSvcClientThunk');
-
 describe('lib/reduxSvcClientThunk', () => {
-  it('should return bar');
+  it('should');
 });
