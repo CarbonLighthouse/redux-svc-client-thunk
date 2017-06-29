cb742d19358478badb2d61fdc0f5175f5c3e42ce
Will Vaughn <willieviseoae@gmail.com>
2017-06-29 14:06:01 -0700

    Update with lodash.get and take an object path to choose service method

--- test/reduxSvcClientThunk.test.js
+++ test/reduxSvcClientThunk.test.js
@@ -1,3 +1,7 @@
 describe('lib/reduxSvcClientThunk', () => {
-  it('should');
+  it('should manage successful service method call as an asynchronous redux thunk action');
+  it('should dispatch GENERIC_SERVICE_FAILURE_ACTION by default on failed service method call');
+  it('should dispatch custom serviceFailureActionType on failed service method call');
+  it('should take actions as functions');
+  it('should build action functions from "type" strings');
 });
